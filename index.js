b1b9ed72d521a2c9f0de2ae69878e9f55b84e01b
unknown <gilbertgalvannn@gmail.com>
2024-01-12 14:29:52 -0800

    Finishes game

--- index.js
+++ index.js
@@ -11,53 +11,71 @@ function getPlayerSelection()
     return prompt("Rock, Paper, or Scissors");
 }
 
-function game(computerSelection, playerSelection)
+function playRound(computerSelection, playerSelection)
 {
     if(playerSelection == computerSelection )
     {
-        console.log("Tie");
+        //console.log("Tie");
+        return "Tie";
     }
     else if(playerSelection == "Rock" && computerSelection == "Paper")
     {
-        console.log("You lost  :(");
+        //console.log("You lost  :(");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You lost  :(";
     }
     else if(playerSelection == "Rock" && computerSelection == "Scissor")
     {
-        console.log("You win!  :)");
+        //console.log("You win!  :)");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You won  :)";
     }
     else if(playerSelection == "Paper" && computerSelection == "Rock")
     {
-        console.log("You win!  :)");
+        //console.log("You win!  :)");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You won  :)";
     }
     else if(playerSelection == "Paper" && computerSelection == "Scissor")
     {
-        console.log("You lose!  :(");
+        //console.log("You lose!  :(");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You lost  :(";
     }
     else if(playerSelection == "Scissors" && computerSelection == "Rock")
     {
-        console.log("You lose!  :(");
+        //console.log("You lose!  :(");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You lost  :(";
     }
     else
     {
-        console.log("You win!  :)");
+        //console.log("You win!  :)");
         console.log("Computer's selection: " + computerSelection + " " + 
         "Player's Selection: " + playerSelection);
+        return "You won  :)";
     }
 
 
 }
 
-let computerSelection = getComputerSchoice();
+function game()
+{   
+    
+    let count = 10;
+    while(count > 0)
+    {
+        let computerSelection = getComputerSchoice();
+        let playerSelection = getPlayerSelection();
+        console.log(playRound(computerSelection,playerSelection));
+        count--;
+    }
+}
 
-let playerSelection = getPlayerSelection();
-game(computerSelection,playerSelection);
+
+console.log(game());
