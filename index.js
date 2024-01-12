1f66513a70cb774ee3827282348c01bb2404edb0
unknown <gilbertgalvannn@gmail.com>
2024-01-11 21:39:38 -0800

    finishes to #4

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-window.prompt("What it do doe pluto?")
+function getComputerSchoice()
+{
+    let choices = ["Rock" , "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    
+    return choices[randomIndex];
+}
+
+function getPlayerSelection()
+{
+    return prompt("Rock, Paper, or Scissors");
+}
+
+function game(computerSelection, playerSelection)
+{
+    if(playerSelection == computerSelection )
+    {
+        console.log("Tie");
+    }
+    else if(playerSelection == "Rock" && computerSelection == "Paper")
+    {
+        console.log("You lost  :(");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+    else if(playerSelection == "Rock" && computerSelection == "Scissor")
+    {
+        console.log("You win!  :)");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+    else if(playerSelection == "Paper" && computerSelection == "Rock")
+    {
+        console.log("You win!  :)");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+    else if(playerSelection == "Paper" && computerSelection == "Scissor")
+    {
+        console.log("You lose!  :(");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+    else if(playerSelection == "Scissors" && computerSelection == "Rock")
+    {
+        console.log("You lose!  :(");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+    else
+    {
+        console.log("You win!  :)");
+        console.log("Computer's selection: " + computerSelection + " " + 
+        "Player's Selection: " + playerSelection);
+    }
+
+
+}
+
+let computerSelection = getComputerSchoice();
+
+let playerSelection = getPlayerSelection();
+game(computerSelection,playerSelection);
